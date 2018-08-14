75cc9f9c8c4bb9550b33cd2452493611df263041
Roma <romansipyak@gmail.com>
2018-08-14 22:52:27 +0300

    correct  authentica

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :email, :password, :password_confirmation,:city_id, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in, keys:[:login, :username, :email, :password, :remember_me])
-    devise_parameter_sanitizer.permit(:sign_out, keys:[:username, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_out, keys:[:username, :email, :password, :password_confirmation,:city_id, :current_password])
   end
 
 
