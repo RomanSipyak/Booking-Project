e4245d264cc62a2f29630a9a72ead581907943c2
Roma <romansipyak@gmail.com>
2018-08-21 12:11:14 +0300

    add filter for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,27 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.includes(:books).where.not(user: current_user)
+    if params[:filter]
+         p container_time[:start_booking]
+         p container_time[:end_booking]
+         p params[:filter][:title]
+         p params[:filter][:city_id]
+         p params[:filter][:category_id]
+         @items = Item.includes(:books)
+                      .by_city(params[:filter][:city_id])
+                      .by_title(params[:filter][:title])
+                      .book_interval(container_time[:start_booking],container_time[:end_booking])
+                      .by_category(params[:filter][:category_id])
+                      .where.not(user: current_user)
+
+         p '1'*100
+        p @items
+         p '1'*100
+
+    else
+      @items = Item.includes(:books).where.not(user: current_user)
+    end
+
   end
 
   def show
@@ -53,6 +73,25 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:description, :category_id, :price)
+    params.require(:item).permit(:description, :category_id, :price,:start_booking)
   end
+
+  def container_time
+    start_booking = DateTime.new(params[:filter]['start_booking(1i)'].to_i,
+                                 params[:filter]['start_booking(2i)'].to_i,
+                                 params[:filter]['start_booking(3i)'].to_i,
+                                 params[:filter]['start_booking(4i)'].to_i,
+                                 params[:filter]['start_booking(5i)'].to_i,
+                                 0)
+    end_booking = DateTime.new(params[:filter]['end_booking(1i)'].to_i,
+                               params[:filter]['end_booking(2i)'].to_i,
+                               params[:filter]['end_booking(3i)'].to_i,
+                               params[:filter]['end_booking(4i)'].to_i,
+                               params[:filter]['end_booking(5i)'].to_i,
+                               0)
+    {start_booking: start_booking, end_booking: end_booking}
+  end
+
+
+
 end
